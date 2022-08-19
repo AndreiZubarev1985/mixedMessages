c1347a3ff56dbbc53a8e2127ba717ff7207f9f64
HP <helena.a.zubareva@gmail.com>
2022-08-19 11:39:32 +0300

    I have modified random function

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const species = ['Rabbit', 'Wolf', 'Bear', 'Human', 'Bison'];
 
-let canEatMeat = true;
-
 const eatMeatInfo = ['I am vegeterian. I do not like another species', 'I am meat eater. I do not know how to eat only leaves and grass', 'I can eat everything. I like honey and fish, and no matter how many to eat'];
 
 const messages = ['I need to find some berries!', 'I need to catch some prey!'];
@@ -9,23 +7,23 @@ const messages = ['I need to find some berries!', 'I need to catch some prey!'];
 
 // Functions 
 
-const getIndexOfSpecies = () => {
-    let indexOfSpecies = Math.floor(Math.random()*species.length);
-    if(indexOfSpecies === 0) {
-     return indexOfSpecies;
+const getIndex = (arr) => {
+    let index = Math.floor(Math.random()*arr.length);
+    if(index === 0) {
+     return index;
     } else {
-     return indexOfSpecies -= 1;
+     return index -= 1;
     }
    }
 
 const printMixedMessage = () => {
-      let indexOfSpecies = getIndexOfSpecies();
-      if(species[indexOfSpecies] === 'Rabbit') {
-        return `Hello I am ${species[indexOfSpecies]}. ${eatMeatInfo[0]}. ${messages[0]}`;
-      } else if(species[indexOfSpecies] === 'Bear') {
-        return `Hello I am ${species[indexOfSpecies]}. ${eatMeatInfo[eatMeatInfo.length-1]}. ${messages[messages.length-1]}`;
+      let index = getIndex(species);
+      if(species[index] === 'Rabbit') {
+        return `Hello I am ${species[index]}. ${eatMeatInfo[0]}. ${messages[0]}`;
+      } else if(species[index] === 'Bear' || species[index] === 'Human') {
+        return `Hello I am ${species[index]}. ${eatMeatInfo[eatMeatInfo.length-1]}. ${messages[getIndex(messages)]}`;
       } else {
-        return `Hello I am ${species[indexOfSpecies]}. ${eatMeatInfo[1]}. ${messages[1]}`;
+        return `Hello I am ${species[index]}. ${eatMeatInfo[1]}. ${messages[1]}`;
       }
 }
 
